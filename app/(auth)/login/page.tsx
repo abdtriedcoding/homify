6184a1f1ae8035817c2feb81757518866222530f
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-11 23:57:48 +0530

    add login buttons

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="py-56 flex flex-col space-y-4 items-center text-center justify-center">
+      <Button size={"lg"}>Login With Google</Button>
+      <Button size={"lg"}>Login With Github</Button>
+    </div>
+  );
 };
 
 export default page;
