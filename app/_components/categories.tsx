cd250d5adb336e91e7c474ee5f0c170e015703ae
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-12 18:11:07 +0530

    add category click functionality

--- app/_components/categories.tsx
+++ app/_components/categories.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import CategoryBox from "@/app/_components/category-box";
 import { categories } from "@/constants";
 
 const Categories = () => {
   return (
-    <div className="p-2 flex flex-row items-center justify-between overflow-x-auto scrollbar-hide">
+    <div className="p-4 flex flex-row items-center justify-between overflow-x-auto scrollbar-hide">
       {categories.map((item) => (
         <CategoryBox key={item.label} label={item.label} icon={item.icon} />
       ))}
--- app/_components/category-box.tsx
+++ app/_components/category-box.tsx
@@ -1,8 +1,24 @@
 import { CategoryBoxProps } from "@/types";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const CategoryBox = ({ icon: Icon, label }: CategoryBoxProps) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const category = searchParams?.get("category");
+
+  const selected = category === label;
+
+  const handleClick = () => {
+    router.push(`/?category=${label}`);
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer">
+    <div
+      onClick={handleClick}
+      className={`flex flex-col cursor-pointer items-center justify-center gap-2p-3border-b-2 hover:text-neutral-800transitioncursor-pointer${
+        selected ? "border-b-neutral-800" : "border-transparent"
+      } ${selected ? "text-neutral-800" : "text-neutral-500"}`}
+    >
       <Icon size={26} />
       <div className="font-medium text-sm">{label}</div>
     </div>
