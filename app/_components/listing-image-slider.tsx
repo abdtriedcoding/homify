1fb35d9a09f288df2331478dca0f6d958ebe1c8c
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-15 00:25:53 +0530

    add favourites and share button

--- app/_components/listing-image-slider.tsx
+++ app/_components/listing-image-slider.tsx
@@ -6,10 +6,23 @@ import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import { HeartIcon, UploadIcon } from "lucide-react";
 
 const ListingImageSlider = ({ urls }: { urls: string[] }) => {
   return (
     <div className="group h-96 w-full relative bg-zinc-100 aspect-square overflow-hidden rounded-xl">
+      <div className="flex space-x-2 items-center absolute top-2 right-2 z-10">
+        <div>
+          <button className="bg-white rounded-full p-2">
+            <UploadIcon size={24} />
+          </button>
+        </div>
+        <div>
+          <button className="bg-white rounded-full p-2">
+            <HeartIcon size={24} />
+          </button>
+        </div>
+      </div>
       <Swiper
         navigation
         pagination={{ type: "fraction" }}
