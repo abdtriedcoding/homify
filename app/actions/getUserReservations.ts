d8028244305bfe1565da6b75652bf90b83dbdb0f
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-01-19 00:46:24 +0530

    add function to get user reservation on his property

---
+++ app/actions/getUserReservations.ts
@@ -0,0 +1,34 @@
+import prisma from "@/lib/prismadb";
+
+export default async function getUserReservations(userId: string) {
+  try {
+    const reservations = await prisma.reservation.findMany({
+      where: {
+        listing: {
+          userId: userId,
+        },
+      },
+      include: {
+        listing: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    const safeReservations = reservations.map((reservation) => ({
+      ...reservation,
+      createdAt: reservation.createdAt.toISOString(),
+      startDate: reservation.startDate.toISOString(),
+      endDate: reservation.endDate.toISOString(),
+      listing: {
+        ...reservation.listing,
+        createdAt: reservation.listing.createdAt.toISOString(),
+      },
+    }));
+
+    return safeReservations;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
